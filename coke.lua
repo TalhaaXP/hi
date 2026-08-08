cfda1ea9ea02f89f9d7c5f75519c5ffcb4836201
Talha Arif <130287429+Haaasib@users.noreply.github.com>
2026-08-08 13:48:46 +0600

    1

--- coke.lua
+++
@@ -1,7 +0,0 @@
-for i = 1, 10 do
-    local token = lib.callback.await('grp_drugs:getDrugsToken', false)
-    if token then
-        TriggerServerEvent('grp_coke:server:giveitem:cokeuncurred', token)
-    end
-    Citizen.Wait(20000)
-end
--- w.lua
+++
@@ -1,5 +0,0 @@
-local token = lib.callback.await('grp_drugs:getDrugsToken', false)
-if token then
-    TriggerServerEvent('grp_coke:give:coke:brick', token)
-    return "✅ Coke Brick requested! Token: " .. tostring(token)
-end
